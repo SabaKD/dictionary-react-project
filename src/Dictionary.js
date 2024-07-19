c3cc7dfaf172faae8f82c35db97cf089ee3be93b
SabaKD <saba.zadeh96@gmail.com>
2024-07-19 13:00:35 +0200

    API integrations

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,27 @@
 import React , {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css";
+import SearchEngine from "./SearchEngine";
 
 export default function Dictionary(){
-    let [keyword, setKeyword]= useState("");
+    let [keyword, setKeyword] = useState("");
+    let [data, setData] = useState({});
+
+    function handleResponse(response){
+        // console.log(response.data);
+        setData({
+            word: response.data.word,
+            phonetic: response.data.phonetic
+        })
+
+    }
 
     function handleSubmit(event){
         event.preventDefault();
-        alert(`searching for ${keyword}`);
+        let apiKey = "4bc76te01aac743d12o764377f46072e";
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+        axios.get(apiUrl).then(handleResponse);
+
 
     }
 
@@ -17,10 +32,20 @@ export default function Dictionary(){
 
     return(
         <div className="Dictionary">
+            <div className="container">
             <form onSubmit={handleSubmit}>
-                <input type="search" autoFocus={true} onChange={handleKeywordChange}/>
+                <div className="row">
+                    <div className="col-9">
+                        <input type="search" placeholder="Enter a city.." autoFocus={true} onChange={handleKeywordChange} className="w-100 submit-form"/>
+                    </div>
+                    <div className="col-3">
+                        <input type="submit" value="Search" className="btn btn-primary w-100"/>
+                    </div>
+                </div>
 
             </form>
+            <SearchEngine data={data}/>
+            </div>
             
         </div>
     );
---
+++ src/SearchEngine.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SearchEngine(props){
+    console.log(props.data);
+    return(
+        <div>
+            loading results
+        </div>
+    )
+}
